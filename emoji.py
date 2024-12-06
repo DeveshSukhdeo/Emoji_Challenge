2dde8c3e3288bfb49fec392375eb1a9d359a176b
syedai8 <syeda.islam275@gmail.com>
2024-12-06 08:39:08 -0500

    Update emoji.py

--- emoji.py
+++ emoji.py
@@ -1,1 +1,16 @@
+#Variables 
 
+#Syeda 
+        canvas.draw_circle((width/2, height/2), width/2 - 50, 10, "#c0c0bf","#c0c0bf")
+        canvas.draw_line((150,150),(200,150), 4, "Black") #left eyebrow 
+        canvas.draw_line((400, 150),(450,150), 4, "Black") #right eyebrow
+        canvas.draw_circle((200,250), 50, 2, "Black") #left eye
+        canvas.draw_circle((400,250), 50, 2, "Black") #right eye
+        canvas.draw_line((150,230), (250,230), 4, "Black") #left eyelid
+        canvas.draw_line((350,230), (450,230), 4, "Black") #right eyelid
+        canvas.draw_line((250,350), (350,350), 4, "Black") #mouth
+#Micah
+
+#Devesh
+
+#Towaf

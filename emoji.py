04aacb450f2e623d569b584434d03b0ec0dfc6fa
Towaf H <144939977+TowafH@users.noreply.github.com>
2024-12-06 08:45:28 -0500

    Update emoji.py

--- emoji.py
+++ emoji.py
@@ -1,6 +1,6 @@
 #Variables 
 
-#Syeda 
+#Syeda - Bottom Left Quadrant
         canvas.draw_circle((width/2, height/2), width/2 - 50, 10, "#c0c0bf","#c0c0bf")
         canvas.draw_line((150,150),(200,150), 4, "Black") #left eyebrow 
         canvas.draw_line((400, 150),(450,150), 4, "Black") #right eyebrow
@@ -9,9 +9,9 @@
         canvas.draw_line((150,230), (250,230), 4, "Black") #left eyelid
         canvas.draw_line((350,230), (450,230), 4, "Black") #right eyelid
         canvas.draw_line((250,350), (350,350), 4, "Black") #mouth
-#Micah
+#Micah - Bottom Right Quadrant
 
-#Devesh
+#Devesh - Top Left Quadrant
         canvas.draw_circle((width/4, height/4), width/4 - 25, 10, "Yellow", "Yellow")  
         canvas.draw_circle((width/4 - 55, height/4 - 50), 30, 1, "black", "white")   
         canvas.draw_circle((width/4 + 55, height/4 - 50), 30, 1, "black", "white")   
@@ -19,4 +19,4 @@
         canvas.draw_circle((width/4 + 55, height/4 - 50), 10, 1, "black", "black")  
         canvas.draw_polygon([(width/4 - 100, height/4 + 30), (width/4 - 50, height/4 + 80), (width/4 + 50, height/4 + 80), (width/4 + 100, height/4 + 30)], 1, "black", "white")   
 
-#Towaf
+#Towaf - Top Right Quadrant
